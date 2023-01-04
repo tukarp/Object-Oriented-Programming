2573ad3ef7948a144ea776a33f9a4e6be738c6a8
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-01-04 23:55:11 +0100

    Done

--- Lab 01/Main.java
+++ Lab 01/Main.java
@@ -14,7 +14,6 @@ public class Main {
         Polygon poly = new Polygon(4);
         poly.setPoints(new Point[]{new Point(120,30), new Point(170,180), new Point(240,320), new Point(110,30)});
         System.out.println(poly.toSvg());
-
     }
 }
 
